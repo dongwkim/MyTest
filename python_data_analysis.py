09d21cb185639ff2b2a62f122f91029e548afb77
dongwkim <dotorry@gmail.com>
2016-09-02 13:02:07 +0900

    commit

--- python_data_analysis.py
+++ python_data_analysis.py
@@ -3,3 +3,8 @@ from datetime import datetime,date,time
 dt = datetime(2018, 9, 2, 11, 13)
 print(dt.day, dt.minute)
 print(dt.date())
+dt.strftime
+
+a = [1, 2, 3, 4, 5]
+b = [3, 4, 5, 6, 7]
+print(set(a))
